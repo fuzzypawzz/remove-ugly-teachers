61c7ab7151155e505ebb03f490fc519776c878b3
J. Maag <55883716+fuzzypawzz@users.noreply.github.com>
2021-12-23 17:29:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,57 @@
 (function () {
-    const errorMessage = "Could not remove the ugly teachers, something must have changed. Please report this error to the developer."
-    const targets = {
-        selectors: [
-            '.clouds',
-            '.laerer-hold-top-grafik',
-            '#kids'
-        ],
-        imageSources: [
-            'forside.svg',
-            'forside_laerere_jul.svg',
-            'forside_laerere.svg',
-            'forside-jul.svg',
-            'laerer'
-        ],
-        elementsWithBackgroundImages: [
-            '.my-week-background-wrapper'
-        ]
+    function removeTeachers() {
+        const errorMessage = "Could not remove the ugly teachers, something must have changed. Please report this error to the developer."
+        const targets = {
+            selectors: [
+                '.clouds',
+                '#kids'
+            ],
+            imageSources: [
+                'forside.svg',
+                'forside_laerere_jul.svg',
+                'forside_laerere.svg',
+                'forside-jul.svg',
+                'laerer'
+            ],
+            elementsWithBackgroundImages: [
+                '.my-week-background-wrapper'
+            ]
+        }
+
+        try {
+            const images = document.querySelectorAll('img')
+            images.forEach(img => {
+                targets.imageSources.forEach(src => {
+                    if (img.src.includes(src)) img.remove()
+                })
+            })
+
+            targets.selectors.forEach(selector => {
+                const matchingNodes = document.querySelectorAll(selector)
+                matchingNodes.forEach(n => n.remove())
+            })
+
+            targets.elementsWithBackgroundImages.forEach(selector => {
+                const matchingNodes = document.querySelectorAll(selector)
+                console.log(matchingNodes)
+                matchingNodes.forEach(n => {
+                    n.style.backgroundImage = 'url()'
+                })
+            })
+
+        } catch (e) {
+            console.warn(errorMessage);
+            console.error('Remove ugly teachers plugin: ' + e)
+        }
     }
 
-    try {
-        const images = document.querySelectorAll('img')
-        images.forEach(img => {
-            targets.imageSources.forEach(src => {
-                if (img.src.includes(src)) img.remove()
-            })
-        })
+    removeTeachers()
 
-        targets.selectors.forEach(selector => {
-            const matchingNodes = document.querySelectorAll(selector)
-            matchingNodes.forEach(n => n.remove())
-        })
-
-        targets.elementsWithBackgroundImages.forEach(selector => {
-            const matchingNodes = document.querySelectorAll(selector)
-            matchingNodes.forEach(n => {
-                n.style.backgroundImage = 'url()'
-            })
-        })
-
-    } catch (e) {
-        console.warn(errorMessage);
-        console.error('Remove ugly teachers plugin: ' + e)
-    }
+    // Fallback for slow loading content some places
+    const intervals = [200, 700, 1200]
+    intervals.forEach(interval => {
+        setTimeout(() => {
+            removeTeachers()
+        }, interval);
+    })
 })()
